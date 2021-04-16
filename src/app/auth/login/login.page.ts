6650a40002ba1f90b96fd353c1eb8f2ab92617a6
lyoman <leomuchenje@gmail.com>
2021-04-17 00:55:20 +0200

    dome login

--- src/app/auth/login/login.page.ts
+++ src/app/auth/login/login.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AlertController, NavController } from '@ionic/angular';
+import { AuthService } from './../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -8,10 +10,18 @@ import { Router } from '@angular/router';
 })
 export class LoginPage implements OnInit {
 
-  user = { id: '', username: '', password: ''};
+  user = { remember_me: 1, email: '', password: ''};
+
+  loading: any;
+
+  who = "";
+  error: any;
 
   constructor(
-    private router: Router
+    private router: Router,
+    private alertController: AlertController,
+    private authService: AuthService,
+    private navCtrl: NavController
   ) { }
 
   ngOnInit() {
@@ -21,12 +31,63 @@ export class LoginPage implements OnInit {
     this.router.navigateByUrl('/guardian');
   }
 
+  // login() {
+  //   this.router.navigateByUrl('/home');
+  // }
+
   login() {
-    this.router.navigateByUrl('/home');
+    if (this.user.email == "" || this.user.password == "" || this.who == "") {
+      this.presentAlert1();
+    }
+    else {
+      // this.loading.presentLoading();
+      this.loading = true;
+      this.authService.login('login', this.user).subscribe((res) => {
+        localStorage.setItem('token', res.access_token);
+        localStorage.setItem('loggedIn', 'true');
+        console.log(res);
+        this.loading = false;
+        if(this.who == 'user') {
+          this.navCtrl.navigateRoot('home');
+        } else {
+          this.navCtrl.navigateRoot('tebs');
+        }
+      }, (err) => {
+        console.log(err);
+        this.error = err;
+        // this.loading.dismiss();
+        this.loading = false;
+        this.presentAlert(err.message);
+      });
+    }
   }
 
   register() {
     this.router.navigateByUrl('/register');
+  }
+
+  presentAlert2() {
+    const alert = this.alertController.create({
+    header: 'Notice',
+    message: 'To register, visit the website',
+    subHeader: 'website link http://blindstick.herokuapp.com/register',
+    buttons: ['Dismiss']}).then(alert=> alert.present());
+  }
+
+  presentAlert(err) {
+    const alert = this.alertController.create({
+    header: 'Authentication Error!',
+    message: err,
+    subHeader: 'Failed to Login',
+    buttons: ['Dismiss']}).then(alert=> alert.present());
+  }
+
+  presentAlert1() {
+    const alert = this.alertController.create({
+    header: 'Error!',
+    message: 'Please fill in all fields in order to login',
+    subHeader: 'Fill in all fields',
+    buttons: ['Dismiss']}).then(alert=> alert.present());
   }
 
 }
